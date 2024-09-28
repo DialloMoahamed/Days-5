2c75e280c32ff49c6e32d61fe9165442bc13219e
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-09-28 23:25:46 +0100

    Debut de l'exo2.js:Question 1 et 2 terminer:1.Créez un fichier countries.js distinct et stockez le tableau countries dans ce fichier, créez un fichier web_techs.js distinct et stockez le tableau webTechs dans ce fichier. Accédez aux deux fichiers dans le fichier main.js,2.Supprimez d'abord toutes les ponctuations et changez la chaîne en tableau et comptez le nombre de mots dans le tableau

---
+++ Exo2.js
@@ -0,0 +1,9 @@
+// Reponse numero 1
+// Voir dans les fichiers countries.js,web_techs.js
+
+// Reponse numero 2
+let text =
+'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+let words = text.split(' ');
+console.log(words)
+console.log(words.length)
