3766840f874515fcca568c429115a477be2f1ee6
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-09-29 12:55:03 +0100

    Question 3 terminer:Dans le panier suivant, ajoutez, supprimez, modifiez des articles
    const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
    ajoutez « Viande » au début de votre panier si elle n'a pas déjà été ajoutée
    ajoutez du sucre à la fin de votre panier s'il n'a pas déjà été ajouté
    supprimez « Miel » si vous êtes allergique au miel
    modifier le thé en « thé vert »

--- Exo2.js
+++ Exo2.js
@@ -7,3 +7,19 @@ let text =
 let words = text.split(' ');
 console.log(words)
 console.log(words.length)
+
+
+// Reponse numero 3
+//----------- Ajout au debut du tableau -----------//
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+let ajout = shoppingCart.unshift('Viande');
+console.log(ajout);
+//----------- Ajout à la fin du tableau -----------//
+let ajouter = shoppingCart.push('sucre');
+console.log(ajouter);
+//----------- Remove -----------//
+let remove = shoppingCart.splice(4, 1);
+console.log(remove);
+//----------- Replace -----------//
+let replace = shoppingCart.splice(3, 1,'thé vert')
+console.log(replace);
