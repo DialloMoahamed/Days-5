f71cab1ba5434fa9b1443adcbf651ac21ae2ebc7
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-09-30 16:05:03 +0100

    Question 2 modifier

--- Exo2.js
+++ Exo2.js
@@ -3,7 +3,8 @@
 
 // Reponse numero 2
 let text =
-'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.';
+text = text.replace(/[.,]/g, '')
 let words = text.split(' ');
 console.log(words)
 console.log(words.length)
